87cbde431e141e6c14d6d6ac3a4f46a79c9f6619
anton whalley <anton@venshare.com>
2017-01-04 16:44:10 +0000

    home path is fixed so added it to conf

--- run.sh
+++ run.sh
@@ -9,28 +9,7 @@ echo "KORE_PATH: $KORE_PATH"
 
 cd kore-bp
 
-cat << EOF > $PWD/conf/build.conf
-single_binary=yes
-kore_source=$KORE_PATH
-kore_flavor=NO-TLS=1
-  
-# The flags below are shared between flavors
-cflags=-Wall -Wmissing-declarations -Wshadow
-cflags=-Wstrict-prototypes -Wmissing-prototypes
-cflags=-Wpointer-arith -Wcast-qual -Wsign-compare
-
-cxxflags=-Wall -Wmissing-declarations -Wshadow
-cxxflags=-Wpointer-arith -Wcast-qual -Wsign-compare
-
-ldflags=-lcrypto
-EOF 
-
-
-sleep 1
-
 kore build
-
-sleep 1
 
 ./kore-bp -nr
 
